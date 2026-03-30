3759357b02ba82fef077a0c97e575e51b5e92b87
Aftab Khan <59370273+aftab404@users.noreply.github.com>
2026-03-30 15:38:27 +0200

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string s = "";
+        for(string t: strs){
+					if (t.size() < 10) {
+						s += "00" + to_string(t.size()) + t;
+ 				} else if (t.size() < 100) {
+						s += "0" + to_string(t.size()) + t;
+					} else {
+						s += to_string(t.size()) + t;
+					}
+        }
+        return s;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        int idx = 0;
+				int n = s.size();
+        while(idx < n){
+            string int_s = "";
+            int curr_size = stoi(s.substr(idx,3));
+            for(int i = 1; i<=curr_size ; i++){
+                int_s += s[idx + 2 + i];
+            }
+            idx += curr_size + 3;
+            res.push_back(int_s);
+        }
+
+        return res;
+
+    }
+};
