e9fd7f638554c48a3784a7d5fc1eca9b99e490d1
Aftab Khan <59370273+aftab404@users.noreply.github.com>
2026-04-03 13:43:57 +0200

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(char c: s){
+            if(c == '[' || c == '(' || c == '{'){
+                stk.push(c);
+            }else if(c == ')'){
+                if(stk.empty()){
+                    return false;
+                }
+                char popped = stk.top();
+                if(popped != '('){
+                    return false;
+                }
+                stk.pop();
+            }else if(c == '}'){
+                if(stk.empty()){
+                    return false;
+                }
+                char popped = stk.top();
+                if(popped != '{'){
+                    return false;
+                }
+                stk.pop();
+            }else if(c == ']'){
+                if(stk.empty()){
+                    return false;
+                }
+                char popped = stk.top();
+                if(popped != '['){
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+
+        return stk.empty();
+    }
+};
