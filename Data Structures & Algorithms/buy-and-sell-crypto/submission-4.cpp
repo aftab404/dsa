37d43f2eb67cac51fc9066b0aebcf5db833a4fee
Aftab Khan <59370273+aftab404@users.noreply.github.com>
2026-04-10 00:06:10 +0200

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int l = 0;
+        int r = n -1;
+
+        int ptr1 = 0;
+        int ptr2 = n -1;
+
+        int res = 0;
+
+       int prev_ptr1 = ptr1;
+int prev_ptr2 = ptr2;
+
+while(ptr1 < ptr2){
+    res = max(prices[r]- prices[l],res);
+
+    prev_ptr1 = ptr1;
+    while(ptr1 < r - 1 && prices[l] <= prices[ptr1]){
+        ptr1++;
+    }
+    if(prices[ptr1] < prices[l]) l = ptr1;
+
+    res = max(prices[r]- prices[l],res);
+
+    prev_ptr2 = ptr2;
+    while(l + 1 < ptr2 && prices[r] >= prices[ptr2]){
+        ptr2--;
+    }
+    if(prices[ptr2] > prices[r]) r = ptr2;
+
+    res = max(prices[r]- prices[l],res);
+
+    // 🔥 minimal safety fix
+    if (ptr1 == prev_ptr1 && ptr2 == prev_ptr2) {
+        ptr2--;
+    }
+}
+
+        return res;
+    }
+};
